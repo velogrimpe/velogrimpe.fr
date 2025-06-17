a3c2bcd4e8c324463bffe22d4f75043763c09fcf
Yoann Couble <yoann.couble@valraiso.fr>
2025-06-17 20:03:19 +0200

    falaises endpoint

---
+++ api/private/falaises.php
@@ -0,0 +1,41 @@
+<?php
+// Check that Authorization header is and equal to config["admin_token"]
+
+// Allow CORS from all origins
+// header('Access-Control-Allow-Origin: https://velogrimpe.fr, https://www.velogrimpe.fr, https://couble.eu, http://localhost:3100');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+$request_headers = apache_request_headers();
+$http_origin = $request_headers['Origin'];
+$allowed_http_origins = [
+  "https://velogrimpe.fr",
+  "https://www.velogrimpe.fr",
+  "https://couble.eu",
+  "http://localhost:3100",
+];
+if (in_array($http_origin, $allowed_http_origins)) {
+  header("Access-Control-Allow-Origin: " . $http_origin);
+}
+
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(200);
+  exit;
+}
+// Allow only GET requests
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+  http_response_code(405);
+  echo json_encode(['error' => 'Method Not Allowed']);
+  exit;
+}
+
+header('Content-Type: application/json');
+
+require_once "../../database/velogrimpe.php";
+// Prepare the SQL statement
+
+$falaises = $mysqli->query("SELECT falaise_id, falaise_nom FROM falaises")->fetch_all(MYSQLI_ASSOC);
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  // Return the result as JSON
+  echo json_encode($falaises);
+}
