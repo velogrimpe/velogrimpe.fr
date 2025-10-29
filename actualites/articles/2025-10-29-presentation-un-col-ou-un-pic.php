174f43f7a8d586dc7205e1cc848fcedd7d72c353
Yoann Couble <yoann.couble@valraiso.fr>
2025-10-29 15:57:19 +0100

    article uncolunpic

--- actualites/articles/2025-10-29-presentation-un-col-ou-un-pic.php
+++ actualites/articles/2025-10-29-presentation-un-col-ou-un-pic.php
@@ -14,7 +14,7 @@
   <meta property="og:title" content="Velogrimpe.fr x Un Col ou Un Pic">
   <meta property="og:type" content="website">
   <meta property="og:site_name" content="Velogrimpe.fr">
-  <meta property="og:url" content="https://velogrimpe.fr/2025-10-29-presentation-un-col-ou-un-pic.php">
+  <meta property="og:url" content="https://velogrimpe.fr/actualites/2025-10-29-presentation-un-col-ou-un-pic.php">
   <meta property="og:image" content="https://velogrimpe.fr/images/mw/velogrimpe-social-60.webp">
   <meta property="og:description"
     content="Escalade en mobilité douce à vélo et en train. Découvrez les accès aux falaises, les topos et les informations pratiques pour une sortie vélo-grimpe.">
@@ -24,7 +24,7 @@
   <meta name="twitter:description"
     content="Escalade en mobilité douce à vélo et en train. Découvrez les accès aux falaises, les topos et les informations pratiques pour une sortie vélo-grimpe.">
   <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.23/dist/full.min.css" rel="stylesheet" type="text/css" />
-  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
   <!-- Pageviews -->
   <script async defer src="/js/pv.js"></script>
   <!-- Velogrimpe Styles -->
@@ -34,29 +34,42 @@
 </head>
 
 <body>
-  <?php include "./components/header.html"; ?>
+  <?php include "../../components/header.html"; ?>
   <main class="pb-4">
-    <div class="hero min-h-[400px] bg-bottom" style="background-image: url(/images/mw/078-groupe-5.webp);">
+    <div class="hero min-h-[400px] bg-bottom"
+      style="background-image: url(/images/articles/2025-10-29-presentation-un-col-ou-un-pic/hero.webp);">
       <div class="hero-overlay bg-opacity-60"></div>
       <div class="hero-content text-center text-base-100">
-        <div class="max-w-md prose prose-a:text-[oklch(var(--p)/1)] prose-a:font-bold prose-a:no-underline
-              hover:prose-a:underline hover:prose-a:text-[oklch(var(--pf)/1)]">
-          <h1 class="text-5xl font-bold">Stage d’escalade outdoor en mobilité douce avec Un Col ou un Pic</h1>
-          <p>Vous êtes à la recherche d’un séjour qui combine aventure sportive, immersion en pleine nature et
-            engagement éco-responsable ? Découvrez l’agence Un Col ou un Pic, spécialisée dans les stages d’escalade en
-            nature, alliant l’escalade en falaise et la mobilité douce à vélo.</p>
-          <p> Sur leur site <a href="https://www.uncolouunpic.com/">https://www.uncolouunpic.com/</a>, vous trouverez
-            plusieurs formules de stages d’escalade en nature, de 2 à 5 jours, dans des sites emblématiques comme les
-            Dentelles de Montmirail les falaises de Vergisson-Solutré ou les falaises d’Entrelacs avec vue sur le lac du
-            Bourget. Les séjours, en petits groupes de 4 à 10 personnes, garantissent une expérience conviviale,
-            authentique et sécurisée.</p>
-          <p>L’idée est de promouvoir un tourisme doux : se rendre à la falaise à vélo, grimper en plein air et profiter
-            d’un cadre naturel, dans un esprit de respect de l’environnement et de mobilité responsable.</p>
-        </div>
+        <div class="text-5xl font-bold">Présentation : Un Col ou un Pic</div>
       </div>
     </div>
+    <div class="pt-4 max-w-xl mx-auto prose prose-a:text-[oklch(var(--p)/1)] prose-a:font-bold prose-a:no-underline
+              hover:prose-a:underline hover:prose-a:text-[oklch(var(--pf)/1)]
+              ">
+      <h1 class="text-3xl font-bold text-center">Stage d’escalade outdoor en mobilité douce avec Un Col ou un Pic</h1>
+      <p>Vous êtes à la recherche d’un séjour qui combine aventure sportive, immersion en pleine nature et engagement
+        éco-responsable ? Découvrez l’agence Un Col ou un Pic, spécialisée dans les stages d’escalade en nature, alliant
+        l’escalade en falaise et la mobilité douce à vélo.</p>
+      <p> Sur leur site <a href="https://www.uncolouunpic.com/">https://www.uncolouunpic.com/</a>, vous trouverez
+        plusieurs formules de stages d’escalade en nature, de 2 à 5 jours, dans des sites emblématiques comme les
+        Dentelles de Montmirail les falaises de Vergisson-Solutré ou les falaises d’Entrelacs avec vue sur le lac du
+        Bourget. Les séjours, en petits groupes de 4 à 10 personnes, garantissent une expérience conviviale, authentique
+        et sécurisée.</p>
+      <p>L’idée est de promouvoir un tourisme doux : se rendre à la falaise à vélo, grimper en plein air et profiter
+        d’un cadre naturel, dans un esprit de respect de l’environnement et de mobilité responsable.</p>
+      <hr class="mt-2 mb-2" />
+      <p class="italic leading-1 text-sm text-slate-600"><b>Note de l'équipe Velogrimpe.fr :</b> Velogrimpe.fr est un
+        site communautaire permettant de référencer les sites accessible en train + vélo. Avec velogrimpe.fr, nous
+        souhaitons vous donner toutes les clés pour partir en autonomie, mais un site n'est pas toujours suffisant ou
+        suffisamment rassurant. Pour les grimpeurs qui voudraient se faire accompagner dans leurs sorties velo-grimpe,
+        une agence spécialisée dans le domaine existe : Un Col ou un Pic ! <br />Velogrimpe.fr et Un Col ou Un Pic ne
+        sont pas liées commercialement et sont deux organisations indépendantes, mais la proposition commerciale de
+        l'agence est parfaitement alignée avec ce que nous promouvons sur le site, voire complémentaire. </p>
+    </div>
+    </div>
+    </div>
   </main>
-  <?php include "./components/footer.html"; ?>
+  <?php include "../../components/footer.html"; ?>
 </body>
 
 </html>
